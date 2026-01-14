71d245d13cb8fdde27d7e4c2248ef6fa55b06eb7
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-01-14 21:13:20 +0530

    dn

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def start():
     from agent.agent import run_agent
     from rag.vectorstore import push_doc
-
-    query = input("Enter yout query")
-    push_doc()
-    state = run_agent(query)
-    print("\n--- ANSWER ---\n")
-    print(state.answer)
+    
+    while(True):
+        query = input("Enter yout query : ")
+        push_doc()
+        state = run_agent(query)
+        print("ANSWER")
+        print(state.answer)
 
 
 if __name__ == "__main__":
